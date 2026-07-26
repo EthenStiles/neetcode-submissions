c17e60f828200b78444bfdcb58555ad9a447da82
Ethen Stiles <72894586+EthenStiles@users.noreply.github.com>
2026-07-26 09:16:28 -0400

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.cpp
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) 
+    {
+        if (!node) return nullptr;
+        unordered_map<int, Node*> oldNodes;
+        unordered_map<int, Node*> newNodes;
+        dfs(node, oldNodes, newNodes);
+
+        for (auto [i, oldNode] : oldNodes)
+        {
+            Node* newNode = newNodes[i];
+
+            for (Node* neighbor : oldNode->neighbors)
+            {
+                newNode->neighbors.push_back(newNodes[neighbor->val]);
+            }
+        }
+        
+        return newNodes[node->val];
+    }
+
+    void dfs(Node* node, unordered_map<int, Node*>& oldNodes, unordered_map<int, Node*>& newNodes)
+    {
+        if (!node) return;
+        if (oldNodes.contains(node->val)) return;
+
+        oldNodes[node->val] = node;
+        newNodes[node->val] = new Node(node->val);
+
+        for (Node* node : node->neighbors)
+        {
+            dfs(node, oldNodes, newNodes);
+        }
+    }
+};
